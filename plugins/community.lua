36f53a85e237e3eef7c7a5aefbfb502cd09816c5
tokcide <kanishk.jurel@gmail.com>
2023-06-09 12:44:12 +0530

    Changes to be committed: 	modified:   init.lua 	modified:   mappings.lua 	modified:   plugins/community.lua 	modified:   plugins/user.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,7 +5,17 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
-  {import="astrocommunity.pack.rust"}
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,16 +9,33 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  -- TODO: testing the plugin
-  "folke/todo-comments.nvim",
-  requires = "nvim-lua/plenary.nvim",
-  config = function()
-    require("todo-comments").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  end,
-  event = "User AstroFile",
-  keys = { { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" } },
+  {
+    -- TODO: testing the plugin
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+    event = "User AstroFile",
+    keys = { { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" } },
+  },
+  {
+    "booperlv/nvim-gomove",
+    config = function()
+      require("gomove").setup {
+        -- whether or not to map default key bindings, (true/false)
+        map_defaults = true,
+        -- whether or not to reindent lines moved vertically (true/false)
+        reindent = true,
+        -- whether or not to undojoin same direction moves (true/false)
+        undojoin = true,
+        -- whether to not to move past end column when moving blocks horizontally, (true/false)
+        move_past_end_col = false,
+      }
+    end,
+  },
 }
